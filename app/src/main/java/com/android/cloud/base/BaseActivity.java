6377de0495c99dc36b99dac97a618bf8ff241921
wangxiaoyun <wangxiaoyun@zhufaner.com>
2017-11-11 15:26:50 +0800

    解耦

--- app/src/main/java/com/android/cloud/base/BaseActivity.java
+++ app/src/main/java/com/android/cloud/base/BaseActivity.java
@@ -4,15 +4,12 @@ import android.os.Bundle;
 import com.android.cloud.http.httprequestlife.LifeCycleListener;
 import com.trello.rxlifecycle2.components.support.RxAppCompatActivity;
 
-import butterknife.ButterKnife;
-
 public abstract  class BaseActivity extends RxAppCompatActivity {
     public LifeCycleListener mListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(getLayoutId());
-        ButterKnife.bind(this);
     }
     protected abstract int getLayoutId();
     /**
