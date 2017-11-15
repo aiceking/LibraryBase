da6915fc5b5699e10d630858188bbad7938e6ec7
wangxiaoyun <wangxiaoyun@zhufaner.com>
2017-11-15 15:41:25 +0800

    一句话权限申请

---
+++ app/src/main/java/com/android/cloud/help/permissionhelp/GetPermissionListener.java
@@ -0,0 +1,9 @@
+package com.android.cloud.help.permissionhelp;
+
+/**
+ * Created by radio on 2017/11/15.
+ */
+
+public interface GetPermissionListener {
+    void onSuccess();
+}
---
+++ app/src/main/java/com/android/cloud/help/permissionhelp/PermissionType.java
@@ -0,0 +1,17 @@
+package com.android.cloud.help.permissionhelp;
+
+/**
+ * Created by radio on 2017/11/15.
+ */
+
+public enum PermissionType {
+    CALENDAR,
+    CAMERA,
+    CONTACTS,
+    LOCATION,
+    MICROPHONE,
+    PHONE,
+    SENSORS,
+    SMS,
+    STORAGE
+}
